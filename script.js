78bd8b700ec84a549cb730256000403988c02a04
Matt Brown <mdbrown17@crimson.ua.edu>
2022-09-19 14:15:30 -0500

    HTML Finalized

--- script.js
+++ script.js
@@ -29,3 +29,46 @@ function apiSearch() {
       alert("error");
     });
 }
+
+function parallax_height() {
+    var scroll_top = $(this).scrollTop();
+    var sample_section_top = $(".sample-section").offset().top;
+    var header_height = $(".sample-header-section").outerHeight();
+    $(".sample-section").css({ "margin-top": header_height });
+    $(".sample-header").css({ height: header_height - scroll_top });
+}
+parallax_height();
+$(window).scroll(function () {
+    parallax_height();
+});
+$(window).resize(function () {
+    parallax_height();
+});
+
+// ACTION ITEM #5
+function displayTime() {
+
+    var html = "";
+
+    var dt = new Date();
+
+    var hour = dt.getHours();
+    var minute = dt.getMinutes();
+
+    var pod = "AM";
+
+    if (hour > 12) {
+        hour -= 12;
+        pod = "PM";
+    }
+
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+
+    html += hour + ':' + minute + " " + pod;
+
+    document.getElementById('time').innerHTML = html;
+
+}
+
