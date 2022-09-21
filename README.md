c4e26375d85c75ef5a50f8f343d0a5132964ea14
Matt Brown <mdbrown17@crimson.ua.edu>
2022-09-20 20:49:40 -0500

    FINAL

--- README.md
+++ README.md
@@ -35,11 +35,11 @@
   - [X] Replace 'my-api-url' under the ajax call with the url from your search API.
   - [X] Replace 'my-api-key' next to 'Ocp-Apim-Subscription-Key' with the api key from your search API.
   - [X] Write a function that calls the 'apiSearch' function on a click of your search button.
-  - [ ] Write a function that changes the background image of your site on a click of your search engine name.
+  - [X] Write a function that changes the background image of your site on a click of your search engine name.
   - [X] Write a function that gets the current time (formatted HH:MM), loads the result into your 'time' div, and displays the div as a JQuery UI dialog window on a click of your time button.
 - BONUS:
   - [ ] Implement a 'I'm feeling lucky' button. This button should take the user to the first web page returned from your search API.
-  - [ ] Modify the function that changes your background image so that each click cycles through two different images without refreshing the page.
+  - [X] Modify the function that changes your background image so that each click cycles through two different images without refreshing the page.
 - Deploy your site to Azure, following the instructions provided on Blackboard.
 
 > It is best to take this little pieces at a time, step by step, as not to get confused by too many things at once. W3 Schools is an amazing resource, and has examples on everything above if you get stuck.
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var len;
 var results = '';
 
+var logoClicks = 0;
+
 function apiSearch() {
-    console.log("made it here");
   var params = {
     "q": $("#query").val(),
     "count": "50",
@@ -18,9 +19,11 @@ function apiSearch() {
       type: "GET",
     })
     .done(function (data) {
-      len = data.webPages.value.length;
+        len = data.webPages.value.length;
+        results = "";
       for (i = 0; i < len; i++) {
-        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+          results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name +
+              "</a>: " + data.webPages.value[i].snippet + "</p>";
         }
 
         // document.getElementById('searchResults').innerHTML = results;
@@ -66,37 +69,74 @@ function displayTime() {
 
     getTime();
 
-        $("#time").dialog({
-            width: 300,
-            height: 150,
-            show: {
-                effect: "slide",
-                duration: 0
-            },
-            hide: {
-                effect: "slide",
-                duration: 750
-            }
-        });
+    $("#time").dialog({
+        width: 300,
+        height: 150,
+        show: {
+            effect: "slide",
+            duration: 1000 
+        },
+        hide: {
+            effect: "slide",
+            duration: 750
+        }
+    });
 }
-
+function pullResults() {
+    $('#searchResults').dialog({
+        width: 500,
+        height: 500,
+        show: {
+            effect: "slide",
+            duration: 1000
+        },
+        hide: {
+            effect: "slide",
+            duration: 750
+        }
+    });
+}
 
 // ACTION ITEM #13
 function iSearch() {
     apiSearch();
-    function pullResults() {
-        $('#searchResults').dialog({
-            width: 500,
-            height: 500,
-            show: {
-                effect: "slide",
-                duration: 1000
-            },
-            hide: {
-                effect: "slide",
-                duration: 750
+    
+    pullResults();
+}
+
+function feelingLucky() {
+    results = "";
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "cbf2489f528140ab98cc639bc67545fa");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            results = "";
+            for (i = 0; i < len; i++) {
+                results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name +
+                    "</a>: " + data.webPages.value[i].snippet + "</p>";
             }
+
+            // document.getElementById('searchResults').innerHTML = results;
+
+            $('#searchResults').html(results);
+
+            console.log(results);
+
+        })
+        .fail(function () {
+            alert("error");
         });
-    }
     pullResults();
 }
